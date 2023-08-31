3395ac793652ea4c78472e5b78e99196de46f9c4
EstiloCodes <nyanlinnhtun2002@gmail.com>
2023-09-01 02:46:00 +0630

    Game Name Added

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const scorechanger = document.getElementById('score-value');
 const durationbtns = document.querySelectorAll('.btn.btn-outline-primary'); 
 const crocodile = document.getElementById('crocodile');
 const instructions = document.getElementById('instructions');
-const restartbutton = document.getElementById('restart'); // Add restart button
+const restartbutton = document.getElementById('restart'); 
 let timerInterval;
-let gameStarted = false; // Flag to track if the game has started
+let gameStarted = false; 
 let durationSelected = false; 
-let currentScore = 0; // Initialize current score
+let currentScore = 0; 
 
-// Retrieve the highest score from local storage or set it to 0
+
 let highestScore = localStorage.getItem('highestScore') || 0;
 
 function startTimer(durationInSeconds) {
@@ -23,22 +23,22 @@ function startTimer(durationInSeconds) {
 
     if (secondsRemaining === 0) {
       clearInterval(timerInterval);
-      crocodile.style.display = 'none'; // Hide the crocodile when the game ends
-      instructions.style.display = 'block'; // Show instructions when the game ends
+      crocodile.style.display = 'none'; 
+      instructions.style.display = 'block'; 
       
-      // Check if current score is higher than highest score
+      
       if (currentScore > highestScore) {
         highestScore = currentScore;
         localStorage.setItem('highestScore', highestScore);
         Hscorechanger.textContent = highestScore;
       }
       
-      currentScore = 0; // Reset current score
+      currentScore = 0; 
     } else {
       secondsRemaining--;
     }
     
-    // Call popUpCrocodileRandomly only if the game has started and duration is selected
+    
     if (gameStarted && durationSelected) {
       popUpCrocodileRandomly();
     }
@@ -48,15 +48,15 @@ function startTimer(durationInSeconds) {
   timerInterval = setInterval(updateTimer, 1000);
 }
 
-crocodile.style.display = 'none'; // Hide the crocodile initially
+crocodile.style.display = 'none'; 
 
 durationbtns.forEach(button => {
   button.addEventListener('click', () => {
     if (!durationSelected) {
-      instructions.style.display = 'none'; // Hide the instructions
-      crocodile.style.display = 'inline-block'; // Show the crocodile when the game starts
-      gameStarted = true; // Set the gameStarted flag to true
-      durationSelected = true; // Set the durationSelected flag to true
+      instructions.style.display = 'none'; 
+      crocodile.style.display = 'inline-block'; 
+      gameStarted = true; 
+      durationSelected = true; 
       const durationInSeconds = parseInt(button.getAttribute('data-duration'), 10);
       startTimer(durationInSeconds);
     }
@@ -71,7 +71,7 @@ restartbutton.addEventListener('click', () => {
   durationSelected = false;
   time.textContent = '0:00';
   scorechanger.textContent = '0';
-  currentScore = 0; // Reset current score
+  currentScore = 0; 
 });
 
 crocodile.addEventListener('click', handleCrocodileClick);
@@ -81,7 +81,7 @@ function popUpCrocodileRandomly() {
   const maxWidth = viewportWidth - crocodile.clientWidth;
 
   const randomLeft = Math.random() * maxWidth;
-  const randomBottom = Math.random() * 200; // Adjust this value for the desired vertical position
+  const randomBottom = Math.random() * 200; 
 
   crocodile.style.left = `${randomLeft}px`;
   crocodile.style.bottom = `${randomBottom}px`;
@@ -92,12 +92,12 @@ function popUpCrocodileRandomly() {
 function handleCrocodileClick() {
   playWaterSplashSound();
   increaseScore();
-  crocodile.style.display = 'none'; // Hide the crocodile after clicking
+  crocodile.style.display = 'none'; 
 
   setTimeout(() => {
     crocodile.style.display = 'block';
     popUpCrocodileRandomly();
-  }, 1000); // Show the crocodile after a delay
+  }, 1000); 
 }
 
 function playWaterSplashSound() {
@@ -110,7 +110,7 @@ function increaseScore() {
   currentScore += 1;
   scorechanger.textContent = currentScore;
 
-  // Update highest score if the current score is higher
+  
   if (currentScore > highestScore) {
     highestScore = currentScore;
     localStorage.setItem('highestScore', highestScore);
@@ -118,8 +118,8 @@ function increaseScore() {
   }
 }
 
-// Set initial highest score value
+
 Hscorechanger.textContent = highestScore;
 
-// Initial call to start the game
+
 
